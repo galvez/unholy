f4127c2ca8dd6cd83d22c9a7e1b43e647dd80856
Jonas Galvez <jonasgalvez@gmail.com>
2019-05-25 11:29:14 -0300

    ongoing refactoring (3)

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
-const { resolve } = require('path')
+const { readFileSync } = require('fs')
+const { resolve, dirname, separator } = require('path')
+
+const fileName = 'store.js'
+const vueAppPath = require.resolve('@nuxt/vue-app')
+const vueAppDistSuffix = p('/dist/vue-app.js')
+const vueAppStoreBase = vueAppPath.slice(0, vueAppPath.length - vueAppDistSuffix)
+const createStoreRegex = /\/\/ createStore[\0-\uFFFF]*?}\n/
+
+function p(path) {
+  return path.replace(/\//g, separator)
+}
 
 function resolvePath(...args) {
-  return resolve(__dirname, ...args)
+  return resolve(__dirname, ...args.map(a => p(a)))
 }
 
-function isObject(val) {
-  return val !== null && typeof val === 'object' && !Array.isArray(val)
+function prepareStoreTemplate() {
+  const vueAppStore = readFileSync(p(`${vueAppStoreBase}/templates/store.js`))
+  const createStore = readFileSync(resolvePath('templates/store.create.js'))
+  const storeOps = readFileSync(resolvePath('templates/store.ops.js'))
+  const storeTemplate = vueAppStore.replace(createStoreRegex, `\n${createStore}`)
+  writeFileSync(resolvePath('templates/store.js'), `${storeTemplate}\n${storeOps}`)
 }
 
-function unholy() {
-  this.addTemplate({
-    src: resolvePath('store.js'),
-    fileName: 'store.js',
-    options: this.options
-  })
+module.exports = function () {
+  const src = resolvePath('templates/store.js')
+  if (!existsSync(src)) {
+    prepareStoreTemplate()  
+  }
+  this.addTemplate({ src, fileName, options: this.options })
 }
-
-module.exports = unholy
---
+++ src/templates/store.ops.js
@@ -0,0 +1,94 @@
+function isObject(val) {
+  return val !== null && typeof val === 'object' && !Array.isArray(val)
+}
+
+function mergeProps(target, source) {
+  if (!isObject(target) || !isObject(source)) {
+    return
+  }
+  for (const key in source) {
+    if (key === '__proto__' || key === 'constructor') {
+      continue
+    }
+    const val = source[key]
+    if (isObject(val) && isObject(target[key])) {
+      mergeObject(target[key], source[key])
+    } else {
+      Vue.set(target, key, val)
+    }
+  }
+}
+
+function anullProps(target, source) {
+  if (!isObject(target) || !isObject(source)) {
+    return
+  }
+  for (const key in source) {
+    if (key === '__proto__' || key === 'constructor') {
+      continue
+    }
+    const val = source[key]
+    if (isObject(val) && isObject(target[key])) {
+      mergeObject(target[key], source[key])
+    } else if (Array.isArray(val)) {
+      for (const vkey in val) {
+        Vue.set(target, vkey, null)
+      }
+    } else {
+      Vue.set(target, key, null)
+    }
+  }
+}
+
+function pushItems(target, source) {
+  if (!isObject(target) || !isObject(source)) {
+    return
+  }
+  for (const key in source) {
+    if (key === '__proto__' || key === 'constructor') {
+      continue
+    }
+    const val = source[key]
+    if (isObject(val) && isObject(target[key])) {
+      pushItems(target[key], source[key])
+    } else if (Array.isArray(val) && Array.isArray(target[key])) {
+      target[key].push(val)
+    }
+  }
+}
+
+function spliceArrays(target, source) {
+  if (!isObject(target) || !isObject(source)) {
+    return
+  }
+  for (const key in source) {
+    if (key === '__proto__' || key === 'constructor') {
+      continue
+    }
+    const val = source[key]
+    if (isObject(val) && isObject(target[key])) {
+      spliceArrays(target[key], source[key])
+    } else if (Array.isArray(val) && Array.isArray(target[key])) {
+      target[key].splice(...val)
+    }
+  }
+}
+
+function emptyArrays(target, source) {
+  if (!isObject(target) || !isObject(source)) {
+    return
+  }
+  for (const key in source) {
+    if (key === '__proto__' || key === 'constructor') {
+      continue
+    }
+    const val = source[key]
+    if (isObject(val) && isObject(target[key])) {
+      emptyArrays(target[key], source[key])
+    } else if (Array.isArray(val)) {
+      for (const vkey in val) {
+        target[vkey].splice(0, target[vkey].length)
+      }
+    }
+  }
+}
